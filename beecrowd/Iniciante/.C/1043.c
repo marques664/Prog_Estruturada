781243c8dc24476d1f4310c3e3b2c22a0b483502
Gustavo Marques <131717010+marques664@users.noreply.github.com>
2024-04-12 13:52:02 -0300

    Create 1043.c

---
+++ beecrowd/Iniciante/.C/1043.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    double a, b, c, area, peri;
+    scanf("%lf %lf %lf", &a, &b, &c);
+    if(a + b > c && a + c > b && b + c > a){
+        peri = a + b + c;
+        printf("Perimetro = %.1lf\n", peri);
+    } else {
+        area = ((a + b)*c) / 2;
+        printf("Area = %.1lf\n", area);
+    }
+    return 0;
+}
