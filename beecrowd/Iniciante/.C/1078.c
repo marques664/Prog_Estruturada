265c3b73a3ac0d45cfbbcfb38a31a6b146713eb6
Gustavo Marques <131717010+marques664@users.noreply.github.com>
2024-08-01 11:47:16 -0300

    Update 1078.c

--- beecrowd/Iniciante/.C/1078.c
+++ beecrowd/Iniciante/.C/1078.c
@@ -1,26 +1,14 @@
+
 #include <stdio.h>
+#include <math.h>
 int main(){
-    int N;
-    scanf("%d", &N);
-    int N1 = N * 1;
-    int N2 = N * 2;
-    int N3 = N * 3;
-    int N4 = N * 4;
-    int N5 = N * 5;
-    int N6 = N * 6;
-    int N7 = N * 7;
-    int N8 = N * 8;
-    int N9 = N * 9;
-    int N10 = N * 10;
-    printf("1 x %d = %d\n", N, N1);
-    printf("2 x %d = %d\n", N, N2);
-    printf("3 x %d = %d\n", N, N3);
-    printf("4 x %d = %d\n", N, N4);
-    printf("5 x %d = %d\n", N, N5);
-    printf("6 x %d = %d\n", N, N6);
-    printf("7 x %d = %d\n", N, N7);
-    printf("8 x %d = %d\n", N, N8);
-    printf("9 x %d = %d\n", N, N9);
-    printf("10 x %d = %d\n", N, N10);
+    int n, res;
+    scanf("%d", &n);
+    for(int i = 1; i <= 10; i++){
+        res = i * n;
+        printf("%d x %d = %d\n", i, n, res);
+    }
     return 0;
 }
+   
+    
