7b402b6e42bd0a40b33b74915f39b8f2b09555fc
Gustavo Marques <131717010+marques664@users.noreply.github.com>
2024-08-12 14:25:31 -0300

    Create 1172.c

---
+++ beecrowd/Iniciante/.C/1172.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int vet[10];
+    for(int i = 0; i < 10; i++){
+        scanf("%d", &vet[i]);
+        if(vet[i] <= 0){
+            vet[i] = 1;
+        }
+        printf("X[%d] = %d\n", i, vet[i]);
+
+    }
+
+    return 0;
+}
