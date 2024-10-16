b19ea5be7bd7a65fdd10b2452a929d2ffd0d3f26
Gustavo Marques <131717010+marques664@users.noreply.github.com>
2024-10-16 09:45:54 -0300

    Create 1184.c

---
+++ beecrowd/Iniciante/.C/1184.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(){
+    float M[12][12], res = 0;
+    char op;
+
+    scanf(" %c", &op);
+
+    for(int i = 0; i < 12; i++){
+        for(int j = 0; j < 12; j++){
+            scanf("%f", &M[i][j]);
+        }
+    }
+
+    int num = 0;
+    for(int i = 0; i < 12; i++){
+        for(int j = 0; j < 12; j++){
+            if(i > j){
+                res += M[i][j];
+                num++;
+            }
+        }
+    }
+    
+
+    switch (op){
+    case 'S': 
+        printf("%.1f\n", res);
+        break;
+    
+    case 'M':
+        res = res / num;
+        printf("%.1f\n", res);
+        break;
+    
+    default:
+        printf("Opcao invalida!\n");
+        break;
+    }
+
+    return 0;
+}
