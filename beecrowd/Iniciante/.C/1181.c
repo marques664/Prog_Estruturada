ed89c63b9bef95c72a8f6c788e2e786c9213b3a9
Gustavo Marques <131717010+marques664@users.noreply.github.com>
2024-10-16 09:27:34 -0300

    Create 1181.c

---
+++ beecrowd/Iniciante/.C/1181.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(){
+    float M[12][12], res = 0;
+    int L = 0;
+    char op;
+
+    scanf("%d", &L);
+    scanf(" %c", &op);
+
+    if(L < 0 || L > 11){
+        printf("Erro: Linha invalida!\n");
+        return 1;
+    }
+
+    for(int i = 0; i < 12; i++){
+        for(int j = 0; j < 12; j++){
+            scanf("%f", &M[i][j]);
+        }
+    }
+
+    for(int i = 0; i < 12; i++){
+        res += M[L][i];
+    }
+
+    switch (op){
+    case 'S': 
+        printf("%.1f\n", res);
+        break;
+    
+    case 'M':
+        res = res / 12;
+        printf("%.1f\n", res);
+        break;
+    
+    default:
+        printf("Opcao invalida!\n");
+        break;
+    }
+
+    return 0;
+}
